171bf533d8af83d86eacd3ed20310fbc510beca8
EGAMAGI <aaggarwal567.aa@gmail.com>
2025-12-18 23:35:14 +0530

    Add product data to data.js
    Added an array of product objects with details including id, category, brand, names in English and Hindi, price, and image.

---
+++ data.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    id: 1,
+    category: "inverter",
+    brand: "Exide",
+    name_en: "Exide Inva Plus 150Ah",
+    name_hi: "एक्साइड इन्वा प्लस 150Ah",
+    price: 12499,
+    image: "exide150.jpg"
+  },
+  {
+    id: 2,
+    category: "car",
+    brand: "Amaron",
+    name_en: "Amaron FLO 65B24",
+    name_hi: "अमरॉन FLO 65B24",
+    price: 6200,
+    image: "amaroncar.jpg"
+  },
+  {
+    id: 3,
+    category: "two-wheeler",
+    brand: "Exide",
+    name_en: "Exide Xplore",
+    name_hi: "एक्साइड एक्सप्लोर",
+    price: 2100,
+    image: "bike.jpg"
+  }
+];
